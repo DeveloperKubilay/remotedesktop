1760e7ce30aef56be18c2c42cc6e57be9787eadc
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2023-08-18 16:58:53 +0300

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,15 +1,17 @@
 var machine = "cli1"
-var ipaddr = "http://localhost:3000/"
+var ipaddr = "http://localhost:3000"
 
 const io = require('socket.io-client');
 const screenshot = require('screenshot-desktop')
 const fs = require("fs")
+const sharp = require('sharp');
 const exec = require("child_process").exec
 var kill = require('tree-kill');
 const socket = io(ipaddr,{
     auth:{auth:machine}
 });
 var pid = 0
+var quality = 1
 pid = exec("node autorun.js").pid//example autorun
 socket.on('connect', () => {
 console.log("connected")
@@ -17,12 +19,18 @@ console.log("connected")
   
   socket.on('message', (data) => {
     if(data.machine !=machine) return;
+    if(data.quality){quality = data.quality}
     if(data.screenshot){
         screenshot({format: 'png'}).then((img) => {
-            socket.emit('clientMessage', {
-                screenshot:img.toString("base64"),
-                process:process.env
-            });
+            sharp(img)
+            .jpeg({ quality: quality }).toBuffer((err, outputBuffer) => {
+                socket.emit('clientMessage', {
+                    screenshot:outputBuffer,
+                    time:data.time,
+                    quality:quality,
+                    process:process.env
+                });
+            })
         }).catch((err) => {})
     }else if(data.kill) kill(pid, 'SIGKILL');
     else if(data.exec){
